fc60bf428787b1109c223d4909cfab3df0d19b48
Salman Malik <salmanmalik@Salmans-MBP.attlocal.net>
2023-01-03 20:23:00 -0600

    first iteration is running into deprecated methods from the libraries

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+
+
 import pyttsx3,PyPDF2
 
-pdfreader = PyPDF2.PdfFileReader(open('book.pdf', 'rb'))
+#insert name of your pdf 
+pdfreader = PyPDF2.PdfReader(open('story.pdf', 'rb'))
 speaker = pyttsx3.init()
 
+for page_num in range(len(pdfreader.pages)):
+    text = pdfreader.pages[page_num].extract_text()
+    clean_text = text.strip().replace('\n', ' ')
+    print(clean_text)
+#name mp3 file whatever you would like
+speaker.save_to_file(clean_text, 'story.mp3')
+speaker.runAndWait()
+
+speaker.stop()
